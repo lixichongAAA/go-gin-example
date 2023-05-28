0f798769839d70865413a040626786c82bc0b826
Lxc <1104426357@qq.com>
2023-05-28 16:27:28 +0800

    app

---
+++ pkg/app/request.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	"github.com/astaxie/beego/validation"
+	"github.com/lixichongAAA/go-gin-example/pkg/logging"
+)
+
+// MarkErrors logs error logs
+func MarkErrors(errors []*validation.Error) {
+	for _, err := range errors {
+		logging.Info(err.Key, err.Message)
+	}
+
+	return
+}
